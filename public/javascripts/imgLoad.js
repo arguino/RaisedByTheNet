a58e2af8d59c75000f4918bbd43bbaf50f7f1758
Jean-Raphaël Poulin Arguin <arguinjr@gmail.com>
2017-10-27 07:52:42 -0400

    Now shows Flickr image from an album

--- public/javascripts/imgLoad.js
+++ public/javascripts/imgLoad.js
@@ -1,4 +1,9 @@
-var imgs = ['https://drscdn.500px.org/photo/231554327/m%3D900/v2?user_id=21987859&webp=true&sig=b79f74d71136d0062a5982e0ff7c060ea944cb3acfd87d5a3b2830ff1238633d', 'https://drscdn.500px.org/photo/231554351/m%3D900/v2?user_id=21987859&webp=true&sig=fc8116f5c63191630cf38dff7e135a61f83bacd71f77ff5a9268f5455afca2ae']
+/*
+https://github.com/500px/api-documentation/blob/master/endpoints/photo/GET_photos_id.md
+https://github.com/500px/api-documentation/blob/master/endpoints/galleries/GET_galleries_id_items.md
+https://github.com/500px/500px-js-sdk
+ */
+// var imgs = ['https://drscdn.500px.org/photo/231554327/m%3D900/v2?user_id=21987859&webp=true&sig=b79f74d71136d0062a5982e0ff7c060ea944cb3acfd87d5a3b2830ff1238633d', 'https://drscdn.500px.org/photo/231554351/m%3D900/v2?user_id=21987859&webp=true&sig=fc8116f5c63191630cf38dff7e135a61f83bacd71f77ff5a9268f5455afca2ae']
 var container = document.getElementById('imageContainer');
 var docFrag = document.createDocumentFragment();
 
--- routes/flickr.js
+++ routes/flickr.js
@@ -1,15 +1,28 @@
-var cheerio = require("cheerio");
 var express = require('express');
 var request = require('request');
 var router = express.Router();
 
+function buildFlickrPhotoApi(farm, server, id, secret) {
+    var string = 'https://farm' + farm + '.staticflickr.com/' + server + '/' + id + '_' + secret + '_h.jpg';
+    return string;
+}
+
 router.get('/', function (req, res, next) {
-    request('https://news.ycombinator.com', function (error, response, html) {
-        if (!error && response.statusCode == 200) {
-            console.log(html);
+    request('https://api.flickr.com/services/rest/?method=flickr.photosets.getphotos&format=json&user_id=156166587@N08&photoset_id=72157661594480778&api_key=856b7c68ff0760ad430e10b48c31c7be&format=json&nojsoncallback=1', {json: true}, function (err, response, body) {
+        if (err) {
+            return console.log(err);
         }
+        var photos = body.photoset.photo;
+        var photoUrls = new Array();
+        photos.forEach(function (photo, index) {
+            photoUrls[index] = buildFlickrPhotoApi(photo.farm, photo.server, photo.id, photo.secret);
+        });
+        console.log(photoUrls);
+        res.render('index', {title: 'Heartbreak', tabarnak: photoUrls});
     });
-    res.render('index', {title: 'Express'});
-});
+})
+;
 
 module.exports = router;
+
+
