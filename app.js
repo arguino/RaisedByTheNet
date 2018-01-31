95208977f1b87653f1d63230b5e93c375d475379
arguino <arguinjr@gmail.com>
2018-01-31 17:28:20 -0500

    Code cleanup in Node.JS
    Cleaned up unused dependencies \n Moved project public files repository directly to the build folder of React \n Modified Favicon \n Removed Views for t he templating engine that aren't used anymore

--- app.js
+++ app.js
@@ -2,33 +2,19 @@ const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-
 const index = require('./routes/index');
-const users = require('./routes/users');
 const flickr = require('./routes/flickr');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'client/build', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
-app.use('/flickr', flickr);
-
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.use('/', index);
+app.use('/flickr', flickr);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -45,7 +31,8 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  //TODO Implement error.html
+  res.render('index.html');
 });
 
 module.exports = app;
